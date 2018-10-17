6b9eb949bad67e6b645a57721d4f40ee36a25b84
Malah <malah@neuf.fr>
2018-10-17 16:01:14 +0200

    added generator

---
+++ src/main/java/fr/istic/gm/aodp/activeobject/TrueValueGenerator.java
@@ -0,0 +1,9 @@
+package fr.istic.gm.aodp.activeobject;
+
+/**
+ * The true value generator
+ */
+public interface TrueValueGenerator {
+
+    Integer getTrueValue();
+}
--- src/main/java/fr/istic/gm/aodp/strategy/Diffusion.java
+++ src/main/java/fr/istic/gm/aodp/strategy/Diffusion.java
@@ -1,4 +1,9 @@
 package fr.istic.gm.aodp.strategy;
 
+import fr.istic.gm.aodp.activeobject.Generator;
+
 public interface Diffusion {
+    void execute(Generator generator);
+    void verify();
+    Integer getValue(Generator generator);
 }
