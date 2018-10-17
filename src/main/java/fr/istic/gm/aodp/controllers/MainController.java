c7485ea2d13955c375c9dc587ca8f1ec53e3739d
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-10-17 16:16:31 +0200

    started to develop JFX binding

--- src/main/java/fr/istic/gm/aodp/controllers/MainController.java
+++ src/main/java/fr/istic/gm/aodp/controllers/MainController.java
@@ -1,4 +1,23 @@
 package fr.istic.gm.aodp.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MainController {
+    @FXML
+    private PieChart monitor1;
+
+    @FXML
+    private BarChart<Integer, Integer> monitor2;
+
+    @FXML
+    private BarChart<Integer, Integer> monitor3;
+
+    @FXML
+    private PieChart monitor4;
 }
