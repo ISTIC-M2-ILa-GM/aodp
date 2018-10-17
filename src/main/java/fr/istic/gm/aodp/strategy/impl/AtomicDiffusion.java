d10ef322ae0e88b83876ce3c54e46fdde661741e
Malah <malah@neuf.fr>
2018-10-17 16:02:16 +0200

    initialize diffusion

--- src/main/java/fr/istic/gm/aodp/strategy/impl/AtomicDiffusion.java
+++ src/main/java/fr/istic/gm/aodp/strategy/impl/AtomicDiffusion.java
@@ -1,6 +1,19 @@
 package fr.istic.gm.aodp.strategy.impl;
 
+import fr.istic.gm.aodp.activeobject.Generator;
 import fr.istic.gm.aodp.strategy.Diffusion;
 
 public class AtomicDiffusion implements Diffusion {
+    public void execute(Generator generator) {
+
+    }
+
+    public void verify() {
+
+    }
+
+    @Override
+    public Integer getValue(Generator generator) {
+        return null;
+    }
 }
