8289babd5f49501ab03dba9c841e0c348fa6dfc8
Malah <malah@neuf.fr>
2018-11-14 15:57:59 +0100

    added generator starter

---
+++ src/main/java/fr/istic/gm/aodp/GeneratorStarter.java
@@ -0,0 +1,37 @@
+package fr.istic.gm.aodp;
+
+import fr.istic.gm.aodp.activeobject.Generator;
+import fr.istic.gm.aodp.activeobject.impl.GeneratorImpl;
+import fr.istic.gm.aodp.strategy.Diffusion;
+import fr.istic.gm.aodp.strategy.DiffusionException;
+import fr.istic.gm.aodp.strategy.impl.AtomicDiffusion;
+import lombok.AllArgsConstructor;
+
+import java.util.concurrent.CompletableFuture;
+
+@AllArgsConstructor
+public class GeneratorStarter implements Runnable {
+
+    public static final String DIFFUSION_ERROR = "Diffusion can't be initialized";
+
+    private Class<? extends Diffusion> diffusionClass;
+
+    private int generateEachMs;
+
+    public static CompletableFuture<Void> start(Class<? extends Diffusion> diffusionClass, int generateEachMs) {
+        return CompletableFuture.runAsync(new GeneratorStarter(diffusionClass, generateEachMs));
+    }
+
+    @Override
+    public void run() {
+        try {
+            Generator generator = new GeneratorImpl(diffusionClass.newInstance());
+            while (true) {
+                Thread.sleep(generateEachMs);
+                generator.generate();
+            }
+        } catch (Exception e) {
+            throw new DiffusionException(DIFFUSION_ERROR);
+        }
+    }
+}
--- src/main/java/fr/istic/gm/aodp/Main.java
+++ src/main/java/fr/istic/gm/aodp/Main.java
@@ -1,15 +1,18 @@
 package fr.istic.gm.aodp;
 
+import fr.istic.gm.aodp.strategy.impl.AtomicDiffusion;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/rootPane.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 400));
@@ -19,5 +22,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        CompletableFuture<Void> start = GeneratorStarter.start(AtomicDiffusion.class, 10000);
     }
 }
