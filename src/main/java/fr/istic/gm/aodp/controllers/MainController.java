61c35ecfe73654b7cad4e9746223e9b4cebad165
Gautier Rouleau <gautier.rouleau@gmail.com>
2019-01-22 16:45:50 +0100

    added two other monitors

--- src/main/java/fr/istic/gm/aodp/controllers/MainController.java
+++ src/main/java/fr/istic/gm/aodp/controllers/MainController.java
@@ -15,7 +15,6 @@ import fr.istic.gm.aodp.enums.ChartIdentifier;
 import fr.istic.gm.aodp.memento.impl.MementoFactoryImpl;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
@@ -28,8 +27,7 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-import static fr.istic.gm.aodp.enums.ChartIdentifier.MONITOR_1;
-import static fr.istic.gm.aodp.enums.ChartIdentifier.MONITOR_4;
+import static fr.istic.gm.aodp.enums.ChartIdentifier.*;
 import static javafx.scene.chart.PieChart.Data;
 
 @Getter
@@ -55,10 +53,10 @@ public class MainController implements MonitorObserver {
     private PieChart pieChart1;
 
     @FXML
-    private BarChart<Integer, Integer> barChart2;
+    private PieChart pieChart2;
 
     @FXML
-    private BarChart<Integer, Integer> barChart3;
+    private PieChart pieChart3;
 
     @FXML
     private PieChart pieChart4;
@@ -69,9 +67,17 @@ public class MainController implements MonitorObserver {
 
     private Canal canal1;
 
+    private Canal canal2;
+
+    private Canal canal3;
+
     private Canal canal4;
 
     private Monitor monitor1;
+
+    private Monitor monitor2;
+
+    private Monitor monitor3;
 
     private Monitor monitor4;
 
@@ -81,21 +87,37 @@ public class MainController implements MonitorObserver {
         this.myDiffusion = new CausalDiffusion();
         this.generator = new GeneratorImpl(this.myDiffusion);
         this.monitor1 = new MonitorImpl(MONITOR_1);
+        this.monitor2 = new MonitorImpl(MONITOR_2);
+        this.monitor3 = new MonitorImpl(MONITOR_3);
         this.monitor4 = new MonitorImpl(MONITOR_4);
 
         ScheduledExecutorService updateScheduledExecutorService = Executors.newScheduledThreadPool(2);
         ScheduledExecutorService getUpdateScheduledExecutorService = Executors.newScheduledThreadPool(2);
+
         this.canal1 = new Canal(this.monitor1, updateScheduledExecutorService, getUpdateScheduledExecutorService, 500L);
-        this.canal4 = new Canal(this.monitor4, updateScheduledExecutorService, getUpdateScheduledExecutorService, 2000L);
+        this.canal2 = new Canal(this.monitor2, updateScheduledExecutorService, getUpdateScheduledExecutorService, 800L);
+        this.canal3 = new Canal(this.monitor3, updateScheduledExecutorService, getUpdateScheduledExecutorService, 1000L);
+        this.canal4 = new Canal(this.monitor4, updateScheduledExecutorService, getUpdateScheduledExecutorService, 1200L);
+
         this.generator.attach(this.canal1);
+        this.generator.attach(this.canal2);
+        this.generator.attach(this.canal3);
         this.generator.attach(this.canal4);
+
         this.monitor1.attach(this);
+        this.monitor2.attach(this);
+        this.monitor3.attach(this);
         this.monitor4.attach(this);
 
 
+        // set default values
         this.pieChart1.setStartAngle(0);
+        this.pieChart2.setStartAngle(0);
+        this.pieChart3.setStartAngle(0);
         this.pieChart4.setStartAngle(0);
         this.setMonitor1Value(0);
+        this.setMonitor2Value(0);
+        this.setMonitor3Value(0);
         this.setMonitor4Value(0);
 
         // listener to change the broadcast method
@@ -111,6 +133,8 @@ public class MainController implements MonitorObserver {
             // Create new generator
             this.generator = new GeneratorImpl(this.myDiffusion);
             this.generator.attach(this.canal1);
+            this.generator.attach(this.canal2);
+            this.generator.attach(this.canal3);
             this.generator.attach(this.canal4);
         });
     }
@@ -119,20 +143,22 @@ public class MainController implements MonitorObserver {
         this.generator.generate();
     }
 
-    public void setMonitor1Value(Integer value) {
+    private void setMonitor1Value(Integer value) {
         ObservableList<Data> observableList = createPieChartData(value);
         this.pieChart1.setData(observableList);
     }
 
-    public void setMonitor2Value(Integer value) {
-        // TODO: implement method
+    private void setMonitor2Value(Integer value) {
+        ObservableList<Data> observableList = createPieChartData(value);
+        this.pieChart2.setData(observableList);
     }
 
-    public void setMonitor3Value(Integer value) {
-        // TODO: implement method
+    private void setMonitor3Value(Integer value) {
+        ObservableList<Data> observableList = createPieChartData(value);
+        this.pieChart3.setData(observableList);
     }
 
-    public void setMonitor4Value(Integer value) {
+    private void setMonitor4Value(Integer value) {
         ObservableList<Data> observableList = createPieChartData(value);
         this.pieChart4.setData(observableList);
     }
@@ -152,10 +178,10 @@ public class MainController implements MonitorObserver {
                 this.setMonitor1Value(i);
                 break;
             case MONITOR_2:
-                // TODO: update monitor 2
+                this.setMonitor2Value(i);
                 break;
             case MONITOR_3:
-                // TODO: update monitor 2
+                this.setMonitor3Value(i);
                 break;
             case MONITOR_4:
                 this.setMonitor4Value(i);
